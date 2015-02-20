b39ef9e6b7695c731ed79cc2a684cb60e7678498
Baranzo94 <Liam@Unknown-3c-15-c2-e4-05-78.home>
2015-02-20 17:21:48 +0000

    Basic Setup
    3 scenes (Start, Test and Salad Kingdom (Yes thats right))
    Will .zip and upload to Dropbox for backup

---
+++ IP 3/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	//public GameObject MainMenuMusic;
+
+	public void Start(){
+		//MainMenuMusic = GameObject.Find ("MainMenuMusic");
+		//DontDestroyOnLoad (MainMenuMusic);
+	}
+	/*public void changeScene( string sceneToChangeTo ){
+		if (sceneToChangeTo == "GameTest") {
+						//MainMenuMusic.audio.Stop ();
+				}
+		Application.LoadLevel (sceneToChangeTo);
+
+	}*/
+
+	public void changeScene( int sceneToChangeTo ){
+		Application.LoadLevel (sceneToChangeTo);
+	}
+
+
+}
---
+++ IP 3/Assets/Scripts/DropOff.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropOff : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Touch Object")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
