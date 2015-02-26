dd6ee12ccd78b6810ee1fe53ce19e06d0f21c9e6
Baranzo94 <Liam@Unknown-3c-15-c2-e4-05-78.home>
2015-02-26 18:32:36 +0000

    Fruit is falling !

---
+++ IP 3/Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	public float speed;
+	
+	void Start ()
+	{
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
