ba9cf4a933da9bf576d9c8cf22ed8ae7d60377ef
Baranzo94 <Liam@Unknown-3c-15-c2-e4-05-78.home>
2015-02-28 16:17:11 +0000

    Modifications to player controller

--- IP 3/Assets/Scripts/GameController.cs
+++ IP 3/Assets/Scripts/GameController.cs
@@ -14,6 +14,18 @@ public class GameController : MonoBehaviour
 	{
 		StartCoroutine (SpawnWaves ());
 	}
+
+	void Update() {
+		if (Input.GetMouseButton(0))
+			Debug.Log("Pressed left click.");
+		
+		if (Input.GetMouseButton(1))
+			Debug.Log("Pressed right click.");
+		
+		if (Input.GetMouseButton(2))
+			Debug.Log("Pressed middle click.");
+		
+	}
 	
 	IEnumerator SpawnWaves ()
 	{
