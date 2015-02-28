51895873a298d164b122ee8370b6994b278a9e67
SMCART17 <smcart17@caledonian.ac.uk>
2015-02-28 18:41:53 +0000

    MouseControlWORKS!!!!!!!!!!

--- IP 3/Assets/Scripts/DestroyByBoundry.cs
+++ IP 3/Assets/Scripts/DestroyByBoundry.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroyByBoundry : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+public class DestroyByBoundry : MonoBehaviour
+{
+	void OnTriggerExit(Collider other)
+	{
+		Destroy(other.gameObject);
 	}
 }
---
+++ IP 3/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControl : MonoBehaviour {
+	
+	// Attach to the gameobject
+	
+	float distance = 10;
+	
+	public void OnMouseDrag()
+	{
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		
+		transform.position = objPosition;
+	}
+}
+
